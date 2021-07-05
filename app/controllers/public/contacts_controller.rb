01bc8c82874430906009620a7ef7f3cecac2f420
imaaa34 <oct.imd34@gmail.com>
2021-07-05 06:06:25 +0000

    [Add] views

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -1,2 +1,12 @@
 class Public::ContactsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def complete
+  end
+
 end
--- app/controllers/public/logs_controller.rb
+++ app/controllers/public/logs_controller.rb
@@ -1,2 +1,36 @@
 class Public::LogsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def search
+  end
+
+  def stats
+  end
+
+  def graph
+  end
+
+  def map
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
