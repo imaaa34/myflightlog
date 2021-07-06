6d9965bee1bdd30657fbe97c40a0e3001091f59d
imaaa34 <oct.imd34@gmail.com>
2021-07-06 14:02:22 +0000

    [Add] admin users index/show

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,6 +1,9 @@
 class Admins::UsersController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_user, only: [:show, :edit, :update]
 
   def index
+    @users = User.all
   end
 
   def show
@@ -10,6 +13,21 @@ class Admins::UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to admins_user_path(@user), notice: 'ユーザ情報を更新しました。'
+    else
+      render :edit
+    end
   end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :is_valid)
+    end
 
 end
