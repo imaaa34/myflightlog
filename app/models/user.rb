8e0b840c8c3ea6b30af1b304b47f8f30c011003a
imaaa34 <oct.imd34@gmail.com>
2021-07-08 15:29:01 +0000

    [Fix] user status

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,6 @@ class User < ApplicationRecord
     super && (self.is_valid == true)
   end
 
-  # enum_helpでステータスを日本語化
-  enum status: {beginner: 0, businessman: 1, master: 2, copilot: 3, pilot: 4}
-
   # ユーザ検索機能
   def self.looks(word)
 		@users = User.where(["name LIKE? OR email LIKE?", "%#{word}%", "%#{word}%"])
