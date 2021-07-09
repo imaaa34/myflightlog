b3d24a83a82c5fe840704909240404b9d4b5d32c
imaaa34 <oct.imd34@gmail.com>
2021-07-09 07:44:32 +0000

    [Add] linegraph on graph page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,13 +48,12 @@ $(document).on('turbolinks:load', function( ){
       }
     }
   });
-});
+
 
 
 /* Logs#stats airlinePieChart
 ---------------------------- */
 
-$(document).on('turbolinks:load', function( ){
   var ctx = document.getElementById("airlinePieChart");
   var airlinePieChart = new Chart(ctx, {
     type: 'pie',
@@ -77,6 +76,55 @@ $(document).on('turbolinks:load', function( ){
         //グラフタイトル
         text: '利用した航空会社'
       }
+    }
+  });
+  
+
+
+/* Logs#graph yearLineChart
+---------------------------- */
+
+  var ctx = document.getElementById("yearLineChart");
+  var yearLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+     //データ項目のラベル
+      labels: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      datasets: [
+        {
+         //凡例のラベル
+          label: 'フライト数(2020年)',
+          data: [35, 34, 37, 35, 34], //グラフのデータ
+          borderColor: "rgba(200,112,126,1)",
+          backgroundColor: "rgba(0,0,0,0)"
+        },
+        {
+         //凡例のラベル
+          label: 'フライト数(2021年)',
+          data: [35, 34, 37, 35, 34], //グラフのデータ
+          borderColor: "rgba(80,126,164,1)",
+          backgroundColor: "rgba(0,0,0,0)"
+        }
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+        //グラフタイトル
+        text: 'フライト数グラフ'
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            suggestedMax: 10,  //最大値
+            suggestedMin: 0,  //最小値
+            stepSize: 1, //縦ラベルの数値単位
+            callback: function(value, index, values){
+              return  value +  '回' //縦軸の単位
+            }
+          }
+        }]
+      },
     }
   });
 });
