b5580c0b71ee58d5b68f5c79e5f052509fe0a5b2
imaaa34 <oct.imd34@gmail.com>
2021-07-15 07:08:43 +0000

    [Add] validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,8 +4,8 @@ class Contact < ApplicationRecord
 
   attr_accessor :name, :email, :content
 
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :content, presence: true
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :content, presence: true, length: { maximum: 300 }
 
 end
