103f4eca551472a28d1578f956542fa2a001434f
imaaa34 <oct.imd34@gmail.com>
2021-07-21 12:59:03 +0000

    [Finish] model and system RSpec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -14,6 +14,42 @@ RSpec.describe Contact, type: :model do
         is_expected.to eq false
         expect(contact.errors[:name]).to include("を入力してください")
       end
+
+      it 'is not less than 1' do
+        contact.name = Faker::Lorem.characters(number: 1)
+        is_expected.to eq false
+      end
+
+      it 'is more than 2' do
+        contact.name = Faker::Lorem.characters(number: 2)
+        is_expected.to eq true
+      end
+    end
+
+    context 'email' do
+      it 'is not empty' do
+        contact.email = ""
+        is_expected.to eq false
+        expect(contact.errors[:email]).to include("を入力してください")
+      end
+    end
+
+    context 'content' do
+      it 'is not empty' do
+        contact.content = ""
+        is_expected.to eq false
+        expect(contact.errors[:content]).to include("を入力してください")
+      end
+
+      it 'is not more than 301' do
+        contact.content = Faker::Lorem.characters(number: 301)
+        is_expected.to eq false
+      end
+
+      it 'is less than 300' do
+        contact.content = Faker::Lorem.characters(number: 300)
+        is_expected.to eq true
+      end
     end
   end
 end
